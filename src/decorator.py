7f2ea559c76ac5093dc50ac1f70091549bb8928d
Jumpr15 <chordataosteichthy@gmail.com>
2026-03-03 20:40:10 +1100

    formatting and regex matching

--- src/decorator.py
+++ src/decorator.py
@@ -1,6 +1,6 @@
 import inspect
 
-arr = []
+tool_list = [] # MUST RUN FUNC FINISH TO REGISTER
 
 def inspector_decorator(func):
      def inspector_wrapper(*args, **kwargs):
@@ -10,19 +10,35 @@ def inspector_decorator(func):
           sig = inspect.signature(func)
           args = sig.parameters.values()
           for arg in args:
-               params.append(arg.name)
+               params.append(
+                    arg.name
+               )
                
-          arr.append({
-               func.__name__: params
+          tool_list.append({ ### MANUAL SET
+               "tool_name": func.__name__,
+               "parameter_list": params
           })
           
      return inspector_wrapper
+
+def format_tool_list(tool_list: list) -> list:
+     for tool in tool_list:
+          formatted_tool = f"""
+          tool_name: {tool['tool_name']}
+          params: {tool['parameter_list']}
+          """
 
 @inspector_decorator
 def test_func(a, b):
      print(a + b)
      c = "string"
      return c
+
+@inspector_decorator
+def sub_func(a, b):
+     print(a - b)
+
      
 test_func(2, 2)
-print(arr)
+sub_func(5, 2)
+print(tool_list)
--- src/model.py
+++ src/model.py
@@ -1,3 +1,5 @@
+import re
+
 from openai import OpenAI
 from dotenv import load_dotenv
 
@@ -8,6 +10,17 @@ client = OpenAI()
 system_prompt = """
 
 You are helpful AI assistant.
+
+Calling Tools Procedure:
+
+<Tool_Call>
+{
+     "tool_name": tool name to call,
+     "parameters": {
+          key pair values of parameter and parameter values for all parameters in parameter_list
+     }
+}
+</Tool_Call>
 
 Tool Calls Available:
 {
@@ -27,19 +40,20 @@ messages = [
      },
      {
           "role": "user",
-          "content": "what token do you use to indicate a tool call"
-     },
-     {
-          "role": "user",
-          "content": "how would i detect this token and isolate the insides"
+          "content": "can you perform a tool call using the specified tool calling format, use any args you want"
      }
 ]
 
 res = client.chat.completions.create(
      model="deepseek.v3-v1:0",
-     messages=messages
+     messages=messages,
+     stream=False
 )
 
 res_text = res.choices[0].message.content
-print(res_text)
+# print(res_text)
 
+check_tool_call
+
+regex_match = re.match(r"<Tool_Call>(.*?)</Tool_Call>", res_text, re.DOTALL)
+print(regex_match.group(1).strip())

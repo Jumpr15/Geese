c16bf402c76b439552ff365bd5a66ecdec45dccb
Jumpr15 <chordataosteichthyes@gmail.com>
2026-03-06 09:22:57 +1100

    implement web search tool

---
+++ src/goose_framework/builtin_tools/brave_search/engine.py
@@ -0,0 +1,66 @@
+import httpx
+
+import inspect
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def web_search_tool(query):
+     client = httpx.Client()
+
+     url = "https://api.search.brave.com/res/v1/web/search"
+     headers = {
+          "Accept": "application/json",
+          "Accept-Encoding": "gzip",
+          "X-Subscription-Token": os.getenv("BRAVE_SEARCH_API_KEY")
+     }
+
+     params = {
+          "q": query
+     }
+
+     response = client.get(
+          url, 
+          headers=headers,
+          params=params
+     )
+
+     results = response.json()
+
+     results_list = []
+     if results.get("web", {}).get("results"):
+          results = results["web"]["results"]
+          for result in results:
+               results_list.append(
+                    {
+                         "title": result['title'],
+                         "content": result['description']
+                    }
+               )
+               
+     query_res = {
+          "query": query,
+          "headers": headers, ### hash/fuzz api key later
+          "params": params,
+          "search_results": [
+               results_list
+          ]
+     }
+     
+     tool_name = inspect.currentframe().f_code.co_name
+     
+     arg_list = []
+     sig = inspect.signature(web_search_tool) ### Manually set
+     args = sig.parameters.values()
+     for arg in args:
+          arg_list.append(
+               arg.name
+          )
+
+     res = {
+          "tool_name": tool_name,
+          "parameters": arg_list,
+          "web_search_content": query_res
+     }
+     return res
--- src/goose_framework/goose.py
+++ src/goose_framework/goose.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 
 from model.model_class import Goose
+from builtin_tools.brave_search.engine import web_search_tool
 from builtin_tools.code_exec.exec import create_file_tool, write_to_file_tool, execute_python_file_tool
 from builtin_tools.qdrant.vector_store import Qdrant_Client
 
@@ -16,7 +17,7 @@ qdrant = Qdrant_Client(
 def user_personal_information_query_vector_store(query):
      return qdrant.document_ss_by_id(query)
 
-tool_list = [create_file_tool, write_to_file_tool, execute_python_file_tool, user_personal_information_query_vector_store]
+tool_list = [create_file_tool, write_to_file_tool, execute_python_file_tool, user_personal_information_query_vector_store, web_search_tool]
 
 goose = Goose(
      "deepseek.v3-v1:0",
@@ -25,7 +26,7 @@ goose = Goose(
 )
 
 out = goose.fly(
-     "whats my favourite artist"
+     "perform a web search to retrive the latest daily news"
 )
 
 print(out)
